88160c7c40c3e1f8cb209ba6b0e1aa45c8704671
Fayzulloh <fayzulloh6757@gmail.com>
2024-06-23 15:59:45 +0500

    add some changes to server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
+import ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
 import { Api, Bot, session } from 'grammy';
+import ffmpeg from 'fluent-ffmpeg';
 import dotenv from 'dotenv';
 
 import { starterScene } from '@scene/starter.scene';
@@ -10,10 +12,16 @@ dotenv.config();
 
 function initializer() {
     
-    const botToken = process.env.BOT_TOKEN ? process.env.BOT_TOKEN : 'not_found'
+    const botToken = process.env.BOT_TOKEN || 'not_found'
     
-    const bot = new Bot<BotContext>(botToken);
-    const api = new Api(botToken);
+    const bot = new Bot<BotContext>(botToken, {
+        client: {
+            apiRoot: process.env.BOT_BASE_URL
+        }
+    });
+    const api = new Api(botToken, {
+        apiRoot: process.env.BOT_BASE_URL
+    });
 
     function initializeMiddlewares():void {
         bot.use(
@@ -24,6 +32,10 @@ function initializer() {
         
         bot.use(scenes.manager());
         bot.use(scenes);
+    }
+
+    function initializeFFMPEG():void {
+        ffmpeg.setFfmpegPath(ffmpegInstaller.path);
     }
 
     function libs():void {
@@ -40,6 +52,7 @@ function initializer() {
     }
 
     function runner():void {
+        // initializeFFMPEG();
         initializeMiddlewares();
         initializeScenes();
         libs();
