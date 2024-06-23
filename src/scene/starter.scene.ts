7fa5dc6d525c5dd7a27eea9305fca005f0859b64
Fayzulloh <fayzulloh6757@gmail.com>
2024-06-23 16:00:26 +0500

    add scens

--- src/scene/starter.scene.ts
+++ src/scene/starter.scene.ts
@@ -1,5 +1,16 @@
-export function starterScene(bot: any) {
+import BotContext from "@customTypes/botcontext"
+import { Bot } from "grammy"
+
+export function starterScene(bot: Bot<BotContext>) {
     bot.command('start', (msg: any) => {
-        msg.scenes.enter('register')
+        msg.scenes.enter('start')
     }) 
+
+    bot.on('message:video', async ctx => {
+        ctx.scenes.enter('videoNote')
+    })
+
+    bot.on('message:document', async ctx => {
+        ctx.scenes.enter('videoNote')
+    })
 }
